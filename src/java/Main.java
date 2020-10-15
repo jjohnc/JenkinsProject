aece574b53f85aa68f0a107f642d26466aae76cf
jjohnc <jcarlosjs@hotmail.com>
2020-10-15 16:36:15 -0500

    comit
    Comit git

---
+++ src/java/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+	public static void main(String[] args){
+		System.out.println("Hola");
+		System.out.println("Conexion Test Jenkins file");
+		System.out.println("Conexion Test");
+	}
+}
