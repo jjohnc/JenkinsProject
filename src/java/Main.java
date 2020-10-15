b4fe866dc1cc6d5956cdd2135be1b6d170713232
jjohnc <jcarlosjs@hotmail.com>
2020-10-15 17:10:34 -0500

    commit compilacion jenkins

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,6 +1,6 @@
 public class Main{
 	public static void main(String[] args){
 		System.out.println("Hola");
-		System.out.println("Conexion Test Jenkins ");
+		System.out.println("Conexion Test Jenkins agregando tarea de compilacion");
 	}
 }
