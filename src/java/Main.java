7b1c8043d50667d6f87576659b38b7d6f4383599
jjohnc <jcarlosjs@hotmail.com>
2020-10-15 18:09:04 -0500

    commit fail

--- src/java/Main.java
+++ src/java/Main.java
@@ -2,7 +2,7 @@ public class Main{
 	public static void main(String[] args){
 		System.out.println("Hola");
 		System.out.println("Conexion Test Jenkins agregando tarea de compilacion");		
-		System.out.println("Probando develop branch");
+		System.out.println("Probando develop branch")
 		
 	}
 }
